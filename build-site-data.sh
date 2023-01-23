a870e5ed834ba6e028cefdd8a8f761f11d7c590f
Jocelyn Le Sage <jocelyn@le-sage.com>
2023-01-22 19:14:14 -0500

    Fixed script to also handle repos with new app definitions.

--- build-site-data.sh
+++ build-site-data.sh
@@ -33,13 +33,35 @@ fi
 echo > "$DATA_FILE"
 
 for REPO in $REPOS; do
-    echo "Fetching appdefs.xml from jlesage/$REPO..."
-    curl -s -o "$TMPDIR"/$REPO.appdefs.xml https://raw.githubusercontent.com/jlesage/$REPO/master/appdefs.xml
-    FRIENDLY_NAME="$(cat "$TMPDIR"/$REPO.appdefs.xml | sed -n 's|.*<friendly_name>\(.*\)</friendly_name>|\1|p')"
+    echo "Fetching appdefs from jlesage/$REPO..."
+
+    FRIENDLY_NAME=
+
+    if [ -z "$FRIENDLY_NAME" ]; then
+        set +e
+        curl -f -s -o "$TMPDIR"/$REPO.appdefs.yml https://raw.githubusercontent.com/jlesage/$REPO/master/appdefs.yml
+        RC=$?
+        set -e
+        if [ $RC -eq 0 ]; then
+            FRIENDLY_NAME="$(cat "$TMPDIR"/$REPO.appdefs.yml | sed -n 's|.*friendly_name: \(.*\)|\1|p')"
+        fi
+    fi
+
+    if [ -z "$FRIENDLY_NAME" ]; then
+        set +e
+        curl -f -s -o "$TMPDIR"/$REPO.appdefs.xml https://raw.githubusercontent.com/jlesage/$REPO/master/appdefs.xml
+        RC=$?
+        set -e
+        if [ $RC -eq 0 ]; then
+            FRIENDLY_NAME="$(cat "$TMPDIR"/$REPO.appdefs.xml | sed -n 's|.*<friendly_name>\(.*\)</friendly_name>|\1|p')"
+        fi
+    fi
+
     if [ -z "$FRIENDLY_NAME" ]; then
         echo "  -> File not found.  Skipping."
         continue
     fi
+
     NAME="$(echo $REPO | sed 's|^docker-||')"
     echo "- name: $NAME" >> "$DATA_FILE"
     echo "  friendlyName: $FRIENDLY_NAME" >>  "$DATA_FILE"
